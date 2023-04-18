efb00fba2495634b04f3c1c5ab87541b01c953ff
Uruj Ahmed Syed <118600268+urujahmedsyed@users.noreply.github.com>
2023-04-18 22:01:31 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ const cookieParser = require('cookie-parser');
 const collName = 'images';
 
 
-app.use(cors({origin:["https://ccareclient.onrender.com/","https://ccareserver.onrender.com"],methods:["GET","POST"]}))
+app.use(cors({origin:["https://ccareclient.onrender.com/","https://ccareserver.onrender.com"],methods:["GET","POST"],credentials: true}))
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-  });
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+//   });
 
 mongoose.connect(
     url,
