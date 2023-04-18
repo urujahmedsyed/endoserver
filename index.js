2fc1d19ee8023015f1feede041f4c219b0ebec59
Uruj Ahmed Syed <118600268+urujahmedsyed@users.noreply.github.com>
2023-04-18 22:11:44 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -14,17 +14,16 @@ const collName = 'images';
 
 
 app.use(cors({origin:["https://ccareclient.onrender.com/","https://ccareserver.onrender.com"],methods:["GET","POST"],credentials: true}))
-app.use(cors(corsOptions));
 app.use(express.json())
 app.use(cookieParser())
 
-// app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-//   });
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
 
 mongoose.connect(
     url,
